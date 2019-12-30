be21ebf1477ee441f0ecb4aeaade9b8b23a11b2f
kianaliyan <2818635104@qq.com>
2019-12-30 15:17:37 +0800

    第二次提交

--- day03/src/com/kiyana/deom1/Demo1.java
+++ day03/src/com/kiyana/deom1/Demo1.java
@@ -2,9 +2,8 @@ package com.kiyana.deom1;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		
-		
-
+		//pwe可以查看当前路径
+		//git add 文件夹名称  表示把这个文件夹添加到github本地仓库
 		System.out.println("***********");
 	}
 }
